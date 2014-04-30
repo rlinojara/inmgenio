835ec18807f0a2b77b808441cfe19958cda06b06
rlinojara <rlinojara@gmail.com>
2014-04-30 13:21:22 -0500

    edita

--- application/wyp/controllers/proyecto.php
+++ application/wyp/controllers/proyecto.php
@@ -3,7 +3,44 @@ class Proyecto extends MY_Controller
 {
 	public function listar_proyecto()
 	{		
-		$data['view'] = 'proyecto/proyecto-list';
+		$this->load->library('pagination');
+		$this->load->model('usuario_model');
+		
+		$config['base_url'] = site_url('proyecto/listar_proyecto/');
+		$config['total_rows'] = $this->usuario_model->cantidad_total_usuarios();
+		$config['per_page'] = 1;
+	
+		
+		$config['num_links'] = '2'; 
+
+		$config['prev_link'] = 'anterior';
+		
+		$config['next_link'] = 'siguiente'; 
+		
+		$config['uri_segment'] = '3'; 
+		
+		$config['first_link'] = '<<';
+		
+		$config['last_link'] = '>>';
+		
+		$this->pagination->initialize($config);
+		
+		
+		$parametros = array( 
+							 
+							 intval($this->uri->segment(3,0)),
+							 intval($config['per_page'])
+						   );
+		
+		$data['proyectos'] = $this->usuario_model->
+								  paginacion_usuario($parametros);
+		
+		$data['paginacion'] =  $this->pagination->create_links();
+		
+		$data['pagina'] = $this->uri->segment(3,'');
+		
+		$data['view'] = 'usuario/usuario-list';
+		
 		$this->load->view('index',$data);
 	}
 	
